3561609f7c9106f06c6ee870abec9751de0a0e2c
Hajime Nakagami <nakagami@gmail.com>
2014-08-15 18:59:04 +0900

    add test

--- minipg/test_minipg.py
+++ minipg/test_minipg.py
@@ -65,11 +65,13 @@ class TestMiniPG(unittest.TestCase):
         cur.execute(u"""
             insert into test_basic (b1, i2,i4,i8,dec,dbl,s,dt,t1,t2,t3,t4) values
                 (TRUE, 1, 2, 3, 1.1, 2.1, 'あいうえお', '2001-01-01', '04:05:06.789', '04:05:06.789', '2003-04-12 04:05:06.789', '2003-04-12 04:05:06.789'),
-                (FALSE, 2, 3, 4, 1.2, NULL, 'かきくけこ', 'January 2, 2001', '04:05:06 PST', '04:05:06 PST', '2003-04-12 04:05:06 PST', '2003-04-12 04:05:06 PST'),
+                (FALSE, 2, 3, NULL, 1.2, 2.2, 'かきくけこ', 'January 2, 2001', '04:05:06 PST', '04:05:06 PST', '2003-04-12 04:05:06 PST', '2003-04-12 04:05:06 PST'),
                 (FALSE, 3, 4, 5, 1.3, 2.3, 'さしすせそ', '20010103', '2003-04-12 04:05:06 America/New_York', '2003-04-12 04:05:06 America/New_York', '2003-04-12 04:05:06 America/New_York','2003-04-12 04:05:06 America/New_York')
         """)
         cur.execute("select * from test_basic")
         self.assertEqual(len(cur.fetchall()), 3)
+        cur.execute("select count(*) from test_basic where i8 is NULL")
+        self.assertEqual(cur.fetchone()[0], 1)
         cur.execute("select i4 from test_basic where i2=%s", (1,))
         self.assertEqual(cur.fetchone()[0], 2)
         cur.execute("select i2 from test_basic where s=%s", (u'あいうえお',))
