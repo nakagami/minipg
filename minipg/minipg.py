2ee82daabf1df034b7ec6cd48cf8564f7c256bb6
Hajime Nakagami <nakagami@gmail.com>
2014-08-26 21:15:07 +0900

    fix typo

--- minipg/minipg.py
+++ minipg/minipg.py
@@ -245,7 +245,7 @@ def _decode_column(data, oid, encoding):
         return b''.join([chr(c) for c in ia]) if PY2 else bytes(ia)
     elif oid in (PG_TYPE_TEXT, PG_TYPE_BPCHAR, PG_TYPE_VARCHAR, PG_TYPE_NAME, PG_TYPE_JSON):
         return data
-    elif oid in (PG_TYPE_UNKNOWN):
+    elif oid in (PG_TYPE_UNKNOWN, ):
         DEBUG_OUTPUT('UNKNOWN type')
         return data
     else:
