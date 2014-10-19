57049c86d98e770a6c349db3c0577b0bc3db8276
Hajime Nakagami <nakagami@gmail.com>
2014-10-19 19:58:06 +0900

    fix typo

--- minipg/minipg.py
+++ minipg/minipg.py
@@ -317,7 +317,7 @@ def escape_parameter(v):
             v.tm_year, v.tm_mon, v.tm_mday, v.tm_hour, v.tm_min, v.tm_sec)
     elif t == datetime.datetime:
         return u'%04d-%02d-%02d %02d:%02d:%02d' % (
-            v.year, v.month, v.day, v.hour, v.minite, v.second)
+            v.year, v.month, v.day, v.hour, v.minute, v.second)
     elif t == datetime.date:
         return str(v)
     elif t == datetime.timedelta:
