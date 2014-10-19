f25d6e1faf77445b93d0a98988a0c569530e7852
Hajime Nakagami <nakagami@gmail.com>
2014-10-19 21:15:46 +0900

    datetime date escape

--- minipg/minipg.py
+++ minipg/minipg.py
@@ -316,10 +316,10 @@ def escape_parameter(v):
         return u'%04d-%02d-%02d %02d:%02d:%02d' % (
             v.tm_year, v.tm_mon, v.tm_mday, v.tm_hour, v.tm_min, v.tm_sec)
     elif t == datetime.datetime:
-        return u'%04d-%02d-%02d %02d:%02d:%02d' % (
+        return u"'%04d-%02d-%02d %02d:%02d:%02d'" % (
             v.year, v.month, v.day, v.hour, v.minute, v.second)
     elif t == datetime.date:
-        return str(v)
+        return "'" + str(v) + "'"
     elif t == datetime.timedelta:
         if v.seconds:
             return u"interval '%d second'" % (v.days * 86400 + v.seconds, )
