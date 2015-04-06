6779b2baf12c69ad90b962cc786271d7f83e8912
Hajime Nakagami <nakagami@gmail.com>
2015-04-07 08:15:39 +0900

    datetime decode as isoformat

--- minipg/minipg.py
+++ minipg/minipg.py
@@ -676,7 +676,7 @@ class Connection(object):
             return u'%04d-%02d-%02d %02d:%02d:%02d' % (
                 v.tm_year, v.tm_mon, v.tm_mday, v.tm_hour, v.tm_min, v.tm_sec)
         elif t == datetime.datetime:
-            return "'" + str(v) + "'"
+            return "'" + v.isoformat() + "'"
         elif t == datetime.date:
             return "date '" + str(v) + "'"
         elif t == datetime.timedelta:
