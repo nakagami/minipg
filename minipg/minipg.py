354daf1dd6ee9291426b3da276698c3006454106
Hajime Nakagami <nakagami@gmail.com>
2015-05-01 18:08:55 +0900

    revision number up

--- minipg/minipg.py
+++ minipg/minipg.py
@@ -34,7 +34,7 @@ import collections
 import binascii
 from argparse import ArgumentParser
 
-VERSION = (0, 5, 0)
+VERSION = (0, 5, 1)
 __version__ = '%s.%s.%s' % VERSION
 apilevel = '2.0'
 threadsafety = 1
