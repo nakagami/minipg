9efb3f0a509666b916b68b1c78518f49d3271233
Hajime Nakagami <nakagami@gmail.com>
2018-02-02 16:41:07 +0900

    a bit fix test

--- minipg/test_minipg.py
+++ minipg/test_minipg.py
@@ -130,7 +130,6 @@ false|5|1.300|2.3|ABC's|2001-01-03
         """)
         self.connection.commit()
         # roolback
-        cur.connection.begin()
         cur.execute("insert into test_trans (i2) values (1)")
         cur.execute("select count(*) from test_trans")
         self.assertEqual(cur.fetchone()[0], 1)
@@ -144,7 +143,6 @@ false|5|1.300|2.3|ABC's|2001-01-03
         cur.execute("select count(*) from test_trans")
         self.assertEqual(cur.fetchone()[0], 1)
         self.connection.commit()
-        cur.connection.begin()
         cur.execute("insert into test_trans (i2) values (2)")
         cur.execute("select count(*) from test_trans")
         self.assertEqual(cur.fetchone()[0], 2)
