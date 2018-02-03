1287beec75e6f7fab2379cac41530fe779fa6d4f
Hajime Nakagami <nakagami@gmail.com>
2018-02-03 11:50:05 +0900

    fix set_timezone

--- minipg/minipg.py
+++ minipg/minipg.py
@@ -731,7 +731,7 @@ class Connection(object):
             return cur.fetchone()[0]
 
     def set_timezone(self, timezone_name):
-        self._send_message(b'Q', b"SET TIME ZONE %s\x00" % (timezone_name.encode('ascii')))
+        self._send_message(b'Q', b"SET TIME ZONE '%s'\x00" % (timezone_name.encode('ascii')))
         self.process_messages(None)
         self.tzinfo = pytz.timezone(timezone_name)
 
