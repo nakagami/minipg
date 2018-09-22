0ef5a1ef1c0f7151f35eb3bc8859e8444d86c2c7
Hajime Nakagami <nakagami@gmail.com>
2018-09-22 01:01:10 +0000

    modify proxy tool for SCRAM-SHA-256 authentication

--- misc/pgproxy.py
+++ misc/pgproxy.py
@@ -42,26 +42,83 @@ def asc_dump(s):
     if r:
         print('[' + r + ']')
 
+def _recv_from_server(server_sock):
+    server_head = recv_from_sock(server_sock, 5)
+    server_code = server_head[0]
+    server_ln = int.from_bytes(server_head[1:], byteorder='big') -4
+    server_data = recv_from_sock(server_sock, server_ln)
+    print('S->C', chr(server_code), binascii.b2a_hex(server_data).decode('ascii'), end='')
+    asc_dump(server_data)
+    return server_head, server_data
+
+
+def _recv_from_client(client_sock):
+    client_head = recv_from_sock(client_sock, 5)
+    client_code = client_head[0]
+    client_ln = int.from_bytes(client_head[1:], byteorder='big')
+    client_data = recv_from_sock(client_sock, client_ln-4)
+    print('C->S', chr(client_code), binascii.b2a_hex(client_data).decode('ascii'), end='')
+    asc_dump(client_data)
+    return client_head, client_data
+
+
+def _recv_from_client_tail(server_sock, client_sock):
+    client_tail = recv_from_sock(client_sock, 5)
+    assert client_tail == b'H\x00\x00\x00\x04'
+    return client_tail
+
 
 def parse_message(server_sock, client_sock):
     while True:
-        server_head = recv_from_sock(server_sock, 5)
-        server_code = server_head[0]
-        server_ln = int.from_bytes(server_head[1:], byteorder='big') -4
-        server_data = recv_from_sock(server_sock, server_ln)
-        print('S->C', chr(server_code), binascii.b2a_hex(server_data).decode('ascii'), end='')
-        asc_dump(server_data)
-        client_sock.send(server_head)
-        client_sock.send(server_data)
-        if server_code == 90:
+        server_head, server_data = _recv_from_server(server_sock)
+        client_sock.send(server_head + server_data)
+        if server_head[0] == 90:
             break
-
 
 def read_login_packet(sock):
     head = recv_from_sock(sock, 4)
     ln = int.from_bytes(head, byteorder='big')
     return head + recv_from_sock(sock, ln-4)
 
+
+def parse_login_response(server_sock, client_sock):
+    server_head, server_data = _recv_from_server(server_sock)
+
+    auth = int.from_bytes(server_data[:4], byteorder='big')
+    if auth == 0:
+        print('trust')
+        client_sock.send(server_head + server_data)
+    elif auth == 5:
+        salt = server_data[4:]
+        print('md5', binascii.b2a_hex(salt).decode('ascii'))
+        client_sock.send(server_head + server_data)
+        client_head, client_data = _recv_from_client(client_sock)
+        server_sock.send(client_head + client_data)
+
+    elif auth == 10:
+        print('SASL')
+        client_sock.send(server_head + server_data)
+        client_head, client_data = _recv_from_client(client_sock)
+        server_sock.send(client_head + client_data)
+
+        server_head, server_data = _recv_from_server(server_sock)
+        auth2 = int.from_bytes(server_data[:4], byteorder='big')
+        assert auth2 == 11
+        print('SCRAMFirst')
+        client_sock.send(server_head + server_data)
+        client_head, client_data = _recv_from_client(client_sock)
+        server_sock.send(client_head + client_data)
+
+        server_head, server_data = _recv_from_server(server_sock)
+        auth3 = int.from_bytes(server_data[:4], byteorder='big')
+        assert auth3 == 12
+        print('SCRAMFinal')
+        client_sock.send(server_head + server_data)
+
+        server_head, server_data = _recv_from_server(server_sock)
+        # accept
+        assert int.from_bytes(server_data[:4], byteorder='big') == 0
+        client_sock.send(server_head + server_data)
 
 def proxy_wire(server_name, server_port, listen_host, listen_port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -75,22 +132,17 @@ def proxy_wire(server_name, server_port, listen_host, listen_port):
     print('C->S login ', binascii.b2a_hex(login_packet).decode('ascii'), end='')
     asc_dump(login_packet)
     server_sock.send(login_packet)
+    parse_login_response(server_sock, client_sock)
+
     parse_message(server_sock, client_sock)
 
     while True:
-        client_head = recv_from_sock(client_sock, 5)
-        client_code = client_head[0]
-        client_ln = int.from_bytes(client_head[1:], byteorder='big')
-        client_data = recv_from_sock(client_sock, client_ln-4)
-        server_sock.send(client_head)
-        server_sock.send(client_data)
-        print('C->S', chr(client_code), binascii.b2a_hex(client_data).decode('ascii'), end='')
-        asc_dump(client_data)
-        if chr(client_code) == 'X':
+        client_head, client_data = _recv_from_client(client_sock)
+        server_sock.send(client_head + client_data)
+        if chr(client_head[0]) == 'X':
             break
-        client_tail = recv_from_sock(client_sock, 5)
-        assert client_tail == b'H\x00\x00\x00\x04'
-        server_sock.send(client_tail)
+        _recv_from_client_tail(server_sock, client_sock)
+
         parse_message(server_sock, client_sock)
 
 if __name__ == '__main__':
